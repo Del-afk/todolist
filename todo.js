6a3bf4bcb76a9cbecda06ebd068640e3f6d9f483
del_afk <aramafkhamird@gmail.com>
2022-01-30 18:01:49 +0330

    manythings added

--- todo.js
+++ todo.js
@@ -3,14 +3,24 @@ const taskTitleEl = document.getElementById("newTaskTitle");
 const listEl = document.getElementById("tasks");
 
 triggerEl.addEventListener("click", () => {
-    console.log("hello world");
     let title = taskTitleEl.value;
-    console.log(title);
-    addToList(title);
+
+    if(title == ""){
+        
+    } else {
+        addToList(title);
+        taskTitleEl.value = "";
+    }
 } );
 
 function addToList(boz) {
+    let myDiv = document.createElement("div");
     let myP = document.createElement("p");
+    let myImg = document.createElement("img");
+    myImg.src = "assets/delete.png";
     myP.innerHTML = boz;
-    listEl.append(myP);
+    myDiv.append(myP);
+    myDiv.append(myImg);
+    myDiv.classList.add("writedtasks");
+    listEl.append(myDiv);
 }
